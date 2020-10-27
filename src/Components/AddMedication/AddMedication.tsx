85810a5abaa3f8ffc5b26d93fd587bcd53c0e032
Anthony bassey <a.bassey@live.co.uk>
2020-10-27 15:23:43 +0100

    preping to incorporate timePicker

--- src/Components/AddMedication/AddMedication.tsx
+++ src/Components/AddMedication/AddMedication.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 
-import {StyleSheet, View} from 'react-native'
+import {StyleSheet, Switch, TouchableOpacity, View} from 'react-native'
 import {addMedication} from '~/Store/Actions'
 import {PrimaryBlueButton, H3, Body, TextInput} from '~/Components'
 import {useDispatch} from 'react-redux'
@@ -30,6 +30,8 @@ export const AddMedication = () => {
   const [dose, setDose] = useState('')
   const [administered, setAdministered] = useState('')
 
+  const [reminder, setReminder] = useState<boolean>(true)
+
   return (
     <View style={styles.CONTAINER}>
       <H3 style={styles.TEXT}>Add Medication</H3>
@@ -51,12 +53,33 @@ export const AddMedication = () => {
       </View>
       <View style={{flexDirection: 'row', alignContent: 'space-between'}}>
         <Body style={styles.INPUT_TEXT}>Administered:</Body>
-        <TextInput style={{width: 200}} value={administered} onChangeText={setAdministered} />
+        <TextInput
+          style={{width: 200}}
+          value={administered}
+          onChangeText={setAdministered}
+          placeholder={'orally, injected, etc'}
+        />
       </View>
-      <View style={{paddingBottom: 10}}>
-        <Body style={styles.TEXT}> Administered can be orally, injected, absorbed etc</Body>
+      <View style={{flexDirection: 'row', paddingHorizontal: 20}}>
+        <Body style={{paddingHorizontal: 10}}>
+          Do you want to set a daily reminder for your medication
+        </Body>
+        <Switch
+          value={reminder}
+          onValueChange={() => {
+            setReminder(!reminder)
+            console.log(reminder)
+          }}
+        />
       </View>
-      <View style={{paddingBottom: 20}}>
+      {reminder && (
+        <View style={{flexDirection: 'row', paddingVertical: 20}}>
+          <TouchableOpacity>
+            <Body>click to pick a time for reminder.</Body>
+          </TouchableOpacity>
+        </View>
+      )}
+      <View style={{paddingVertical: 20}}>
         <PrimaryBlueButton
           style={styles.BUTTON}
           title={'Add Medicine'}
