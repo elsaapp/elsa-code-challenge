6d907e70f842d0358729c7119498434f368fc1fb
marceee <marcelinusande@gmail.com>
2022-08-12 17:12:03 +0300

    add medication view medication delete medication from list

--- src/Start.tsx
+++ src/Start.tsx
@@ -3,10 +3,60 @@ import {StyleSheet, View} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context'
 import {useSelector} from 'react-redux'
 import {Colors} from '~/Colors'
-import {Body, H1, SecondaryBlueButton} from '~/Components'
+import {Body, H1, IconButton, SecondaryBlueButton} from '~/Components'
 import {Routes} from '~/Navigation/Routes'
 import type {RootNavigation} from '~/Root'
 import {nameSelector} from '~/Store/Selectors/User'
+import {Icons} from '~/Icons'
+
+type StartProps = {
+  navigation: RootNavigation
+}
+export const Start: React.FC<StartProps> = ({navigation}) => {
+  const name = useSelector(nameSelector)
+
+  return (
+    <SafeAreaView style={styles.background} edges={['top']}>
+      <View style={styles.content}>
+        <View style={styles.info}>
+          <H1>Hello {name}!</H1>
+          <SecondaryBlueButton
+            title={'Change name'}
+            onPress={() => navigation.navigate(Routes.CHANGE_NAME, {name})}
+          />
+        </View>
+        <Body style={styles.help}>
+          {`Welcome to Elsa! Your companion in tracking & managing your medications`}
+        </Body>
+
+        <View
+          style={{
+            flex: 1,
+            padding: 20,
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+          }}>
+          <IconButton
+            icon={Icons.log({color: Colors.red2, size: 30})}
+            style={styles.tiles}
+            onPress={() => {
+              navigation.setOptions({title: 'Add Medication'})
+              navigation.navigate(Routes.ADD_MEDICATION, {title: 'Add Medication'})
+            }}
+          />
+          <IconButton
+            icon={Icons.Categories.medication({color: Colors.red2, size: 30})}
+            style={styles.tiles}
+            onPress={() => {
+              navigation.setOptions({title: 'Medications'})
+              navigation.navigate(Routes.VIEW_MEDICATION, {title: 'medications'})
+            }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
 
 const styles = StyleSheet.create({
   background: {
@@ -23,31 +73,15 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
   },
   help: {
-    marginHorizontal: 24,
+    paddingTop: '20%',
+    paddingHorizontal: 25,
+  },
+  tiles: {
+    borderRadius: 5,
+    width: 100,
+    height: 100,
+    backgroundColor: '#ADD8E6',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 })
-
-type StartProps = {
-  navigation: RootNavigation
-}
-export const Start: React.FC<StartProps> = ({navigation}) => {
-  const name = useSelector(nameSelector)
-  return (
-    <SafeAreaView style={styles.background} edges={['top']}>
-      <View style={styles.content}>
-        <View style={styles.info}>
-          <H1>Hello {name}!</H1>
-          <SecondaryBlueButton
-            title={'Change name'}
-            onPress={() => navigation.navigate(Routes.CHANGE_NAME, {name})}
-          />
-        </View>
-        <Body style={styles.help}>{`Hmm. It would be great if I had a list of my medications here... 🤔
-        
-        
-Please have a look at src/Start.tsx to get started!
-👩‍💻👨‍💻`}</Body>
-      </View>
-    </SafeAreaView>
-  )
-}
--- src/Store/Actions/actions.ts
+++ src/Store/Actions/actions.ts
@@ -4,6 +4,16 @@ export const cleanState = createAction('CLEAN_STATE')
 
 export const changeName = createAction('CHANGE_NAME', action => (name: string) => action({name}))
 
-export const addMedication = createAction('ADD_MEDICATION', action => (medicationName: string) =>
-  action({medicationName})
+export const addMedication = createAction(
+  'ADD_MEDICATION',
+  action => (
+    medicationName: string,
+    substance: string,
+    dosage: string,
+    modeOfAdministration: string
+  ) => action({medicationName, substance, dosage, modeOfAdministration})
+)
+
+export const deleteMedication = createAction('DELETE_MEDICATION', action => (name: string) =>
+  action({name})
 )
